22f5355ff3193b336e3e0e2b07eff50326b341c4
Techhash <hasakelum@gmail.com>
2016-10-23 15:31:05 +0530

    2nd commit

--- app/src/main/java/techhash/sepfinalapp/Main2Activity.java
+++ app/src/main/java/techhash/sepfinalapp/Main2Activity.java
@@ -1,13 +1,46 @@
 package techhash.sepfinalapp;
 
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Toolbar toolbar=(Toolbar)findViewById(R.id.optiontoolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout layout=(DrawerLayout)findViewById(R.id.drawerLayout2);
+        toggle=new ActionBarDrawerToggle(this,layout,R.string.open,R.string.close);
+        layout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setTitle(R.string.mytoolbaroption_title);
+        NavigationView navigationView=(NavigationView)findViewById(R.id.NavigationView2);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
     }
 }
